3a84833048f86a864d125cb7deebf72607a8ec40
Pavel Bariev <bariew@yandex.ru>
2014-09-01 16:31:56 +0400

    changed menu deep

--- params.php
+++ params.php
@@ -1,17 +1,15 @@
 <?php  
 return [
     'menu'  => [
-        'label'    => 'i18n',
+        'label'    => 'Settings',
         'items' => [
             [
-                'label' => 'Languages',
-                'url' => ['/i18n/message-language/index']
-            ],
-            [
-                'label' => 'Translations',
-                'url' => ['/i18n/message/index']
+                'label'    => 'i18n',
+                'items' => [
+                    ['label' => 'Languages', 'url' => ['/i18n/message-language/index']],
+                    ['label' => 'Translations', 'url' => ['/i18n/message/index']],
+                ]
             ],
         ]
-
-    ]
+    ],
 ];
