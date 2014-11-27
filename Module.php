e8a60b6bbef3ac82c56c9e907f39b34ffb604eca
Pavel Bariev <bariew@yandex.ru>
2014-11-27 11:34:03 +0400

    removed params file

--- Module.php
+++ Module.php
@@ -15,6 +15,21 @@ use yii\i18n\I18N;
  */
 class Module extends \yii\base\Module
 {
+    public $params = [
+        'menu'  => [
+            'label'    => 'Settings',
+            'items' => [
+                [
+                    'label'    => 'i18n',
+                    'items' => [
+                        ['label' => 'Languages', 'url' => ['/i18n/message-language/index']],
+                        ['label' => 'Translations', 'url' => ['/i18n/message/index']],
+                    ]
+                ],
+            ]
+        ],
+    ];
+
     public function uninstall()
     {
         \Yii::configure(\Yii::$app, ['components' => ['i18n' => [
